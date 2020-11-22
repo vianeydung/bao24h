4a8f2ead37b077a4d0708d729adb78c045d57811
dungnct <dungnct@datxanh.com.vn>
2020-11-22 17:44:55 +0700

    - save db success

--- main.js
+++ main.js
@@ -1,9 +1,15 @@
 // This is the main Node.js source code file of your actor.
 // It is referenced from the "scripts" section of the package.json file,
 // so that it can be started by running "npm start".
+const mongoose = require('mongoose');
+const config = process.env;
+mongoose.connect(config.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true })
+  .then(console.log('Connected MongoDb'))
+  .catch(console.log)
 
 // Import Apify SDK. For more information, see https://sdk.apify.com/
 const Apify = require('apify');
+const {CrawlModel} = require('./dbModel');
 
 Apify.main(async () => {
     // Get input of the actor.
@@ -21,6 +27,35 @@ Apify.main(async () => {
     // Function called for each URL
     const handlePageFunction = async ({ request, $ }) => {
         console.log(request.url);
+        const model = {};
+        model.url = request.url;
+        model.title = $('#article_title').text();
+        model.newsDate = $('#article_body > div.updTm.updTmD.mrT5').text();
+        model.description = $('#article_sapo').text();
+        const tagsContent = $('#article_body > p');
+        const contents = []
+        tagsContent.each(async (i,elem) => {
+            const lineText = $(elem).text();
+            if(!lineText){
+              $(elem).children().each(async (i, ele) => {
+                if($(ele).is('img')){
+                  contents.push(`{img:${$(ele).attr('src')}}`);
+                }
+              })
+            } else if($(elem).find('strong').text()){
+              console.log('content : strong', lineText);
+              contents.push(`{strong:${lineText}}`);
+            } else {
+              contents.push(`${lineText}`);
+            }
+        });
+        model.contents = contents;
+        try {
+          console.log('save db success');
+          CrawlModel.create(model);
+        } catch (error) {
+          console.log(error);
+        }
         // Add all links from page to RequestQueue
         await Apify.utils.enqueueLinks({
             $,
@@ -32,7 +67,7 @@ Apify.main(async () => {
     const crawler = new Apify.CheerioCrawler({
         requestQueue,
         handlePageFunction,
-        maxRequestsPerCrawl: 100, // Limitation for only 10 requests (do not use if you want to crawl all links)
+        maxRequestsPerCrawl: 5, // Limitation for only 10 requests (do not use if you want to crawl all links)
     });
     // Run the crawler
     await crawler.run();
